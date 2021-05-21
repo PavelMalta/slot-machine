863e53b997a2b55d1cc84f81f1d0c80871495877
PavelMalta <shamashov87@mail.ru>
2021-05-21 14:27:02 +0300

    Initialize commit

---
+++ src/component/keyboard/Keyboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Keyboard = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
---
+++ src/component/monitor/Monitor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Monitor = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
